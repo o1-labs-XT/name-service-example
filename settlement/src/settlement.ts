11901b02122170316bbfed3d331039e480273d68
Coby <coby@o1labs.org>
2024-09-07 00:13:09 -0400

    refactor and adding tests to settlement

--- settlement/src/settlement.ts
+++ settlement/src/settlement.ts
@@ -1,18 +1,18 @@
-import fs from "fs/promises";
-import path from "path";
-
 import {
   NameService,
-  NameRecord,
   StateProof,
   offchainState,
   Mina,
   PrivateKey
 } from "../../contracts/build/src/NameService.js";
+import type { SettlementInputs } from "./types.js";
+import { fetchActions, settle } from "./utils.js";
 
-export { compile, settlementCycle, RETRY_WAIT_SECONDS }
+export { compile, settlementCycle, RETRY_WAIT_MS, MIN_ACTIONS_TO_REDUCE, MAX_RETRIES_BEFORE_REDUCE }
 
-const RETRY_WAIT_SECONDS = 60_000;
+const RETRY_WAIT_MS = Number(process.env.RETRY_WAIT_MS) || 60_000;
+const MIN_ACTIONS_TO_REDUCE = 6;
+const MAX_RETRIES_BEFORE_REDUCE = 100;
 
 type Config = {
   deployAliases: Record<
@@ -27,89 +27,36 @@ type Config = {
     }
   >;
 };
-let configJson: Config = JSON.parse(await fs.readFile("config.json", "utf8"));
-let config = configJson.deployAliases["devnet"];
-let feepayerKeysBase58: { privateKey: string; publicKey: string } = JSON.parse(
-  await fs.readFile(config.feepayerKeyPath, "utf8")
-);
-
-let zkAppKeysBase58: { privateKey: string; publicKey: string } = JSON.parse(
-  await fs.readFile(config.keyPath, "utf8")
-);
-
-let counter = 0;
-let proof: StateProof;
-
-type SettlementInputs = {
-    feePayer: Mina.FeePayerSpec;
-    nameservice: NameService;
-    feepayerKey: PrivateKey;
-    zkAppKey: PrivateKey;
-}
 
 async function settlementCycle({
     feePayer,
     nameservice,
     feepayerKey,
-    zkAppKey
+    zkAppKey,
+    counter=0
 }: SettlementInputs) {
-  try {
-    // let latest_offchain_commitment = await nameservice.offchainState.fetch();
-    /* actionStateRange = {fromActionState: latest_offchain_commitment?.actionState };
-    let result = await Mina.fetchActions(
-        zkAppAddress,
-        actionStateRange
-      );
-    if ('error' in result) throw Error(JSON.stringify(result));
-    let actions = result.reduce((accumulator, currentItem) => {
-        return accumulator + currentItem.actions.reduce((innerAccumulator) => {
-          return innerAccumulator + 1;
-        }, 0);
-      }, 0);
-    */
-    let actions = 1;
-    let shouldSettle = actions > 6 || counter > 10;
-    if (!shouldSettle) {
-        // Delay and try again later
-        counter++;
-        setTimeout(settlementCycle, RETRY_WAIT_SECONDS, feePayer, nameservice, feepayerKey, zkAppKey);
-    } else {
-        console.time("settlement proof");
-        try {
-            proof = await offchainState.createSettlementProof();
-        } finally {
-            console.timeEnd("settlement proof");
-            try {
-                console.log('entered tx scope');
-                let tx = await Mina.transaction(feePayer, async () => {
-                    await nameservice.settle(proof);
-                })
-                await tx.prove();
-                console.log('send transaction...');
-                const sentTx = await tx.sign([feepayerKey,zkAppKey]).send();
-                console.log(sentTx.toPretty());
-                if (sentTx.status === 'pending') {
-                    console.log(`https://minascan.io/devnet/tx/${sentTx.hash}?type=zk-tx`);
-
-                }
-                counter = 0;
-            }
-            catch(error){
-                console.log(error);
-            }
+    try {
+        const actions = await fetchActions(nameservice);
+        let shouldSettle = actions > 0 && (actions > MIN_ACTIONS_TO_REDUCE || counter > MAX_RETRIES_BEFORE_REDUCE);
+        if(actions === 0) { // If there is nothing to reduce, don't call settle, and don't increment the counter
+            setTimeout(settlementCycle, RETRY_WAIT_MS, {feePayer, nameservice, feepayerKey, zkAppKey, counter});
+        } else if (shouldSettle) { // If we should settle the state, then call settle, and reset the cycle to counter = 0
+            await settle({feePayer, nameservice, feepayerKey, zkAppKey});
             counter = 0;
+            setTimeout(settlementCycle, RETRY_WAIT_MS, {feePayer, nameservice, feepayerKey, zkAppKey, counter});
+        } else { // Otherwise, increment the counter and wait for more actions
+            counter++;
+            setTimeout(settlementCycle, RETRY_WAIT_MS, {feePayer, nameservice, feepayerKey, zkAppKey, counter});
         }
+    } catch (error) {
+        console.log(error)
+        // TODO: If there is an error with the logic, this will just keep looping and catching the error, is there a better approach?
+        setTimeout(settlementCycle, RETRY_WAIT_MS, {feePayer, nameservice, feepayerKey, zkAppKey, counter});
     }
-  } catch (error) {
-    // TODO: If there is an error with the logic, this will just keep looping and catching the error, is there a better approach?
-    setTimeout(settlementCycle, RETRY_WAIT_SECONDS, feePayer, nameservice, feepayerKey, zkAppKey);
-  }
 }
 
-async function compile() {
-    let feepayerKey = PrivateKey.fromBase58(feepayerKeysBase58.privateKey);
+async function compile(zkAppKeysBase58: { privateKey: string; publicKey: string }) {
     let zkAppKey = PrivateKey.fromBase58(zkAppKeysBase58.privateKey);
-    let feepayerAddress = feepayerKey.toPublicKey();
     let zkAppAddress = zkAppKey.toPublicKey();
 
     const Network = Mina.Network({
@@ -117,8 +64,6 @@ async function compile() {
     archive: "https://api.minascan.io/archive/devnet/v1/graphql",
     });
     Mina.setActiveInstance(Network);
-
-    const fee = Number(config.fee) * 1e9;
 
     const nameservice = new NameService(zkAppAddress);
     offchainState.setContractInstance(nameservice);
